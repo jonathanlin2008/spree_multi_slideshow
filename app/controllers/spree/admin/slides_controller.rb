49a81eb7ab21fc92dcf60c0fc9fa9ec1a7cb7ae3
jonathanlin2008 <jonathanlin2008@gmail.com>
2012-10-08 00:05:19 +0300

    Update app/controllers/spree/admin/slides_controller.rb

--- app/controllers/spree/admin/slides_controller.rb
+++ app/controllers/spree/admin/slides_controller.rb
@@ -1,15 +1,16 @@
 module Spree
   module Admin
     class SlidesController < ResourceController
-      before_filter :load_data, :only => [:index, :new, :show, :edit]
+      before_filter :load_data, :only => [:index, :new, :show, :edit, :update]
       
       def update_positions
         params[:positions].each do |id, index|
           Spree::Slide.update_all(['position=?', index], ['id=?', id])
         end
 
+        @slide = Spree::Slide.find(:all)
         respond_to do |format|
-          format.html { redirect_to admin_slideshow_type_slides_url(@slideshow_type) }
+          format.html { redirect_to admin_slideshow_type_slides_url(@slideshow_type,@slide) }
           format.js  { render :text => 'Ok' }
         end
       end
